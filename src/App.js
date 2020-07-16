48f027217ef03f4c15b97450991cb566c4b5cbee
Pål Hamel <pal.hamel@gmail.com>
2020-07-16 09:31:22 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Card } from './components/card'
+
+
+export const App = () => {
+  return (
+    <div>
+      Find me in src/app.js!
+      <Card />
+    </div>
+  )
+}
